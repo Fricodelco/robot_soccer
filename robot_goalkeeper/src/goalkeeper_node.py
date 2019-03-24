d0a99f096807c3c38ba68c6d2b47d34fefad3a07
Vladislav_Bakaev <bakaev.98@bk.ru>
2019-03-24 13:33:17 +0300

    Forward

--- robot_goalkeeper/src/goalkeeper_node.py
+++ robot_goalkeeper/src/goalkeeper_node.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python
 import roslib; roslib.load_manifest('robot_goalkeeper')
 import rospy
-from std_msgs.msg import Int32
+from std_msgs.msg import Int16
 from ball_param_msg.msg import ball_param
 def callback(data):
+	pub_action = rospy.Publisher('forward_action', Int16, queue_size=10)
+	pub_y_err = rospy.Publisher('forward_y_err',Int16, queue_size=10)
+	action = Int16()
 	rospy.loginfo(data.find_ball)
-	pub = rospy.Publisher('goalkeeper_action', Int32, queue_size=20)
-	value = Int32()
-	limit_close_distance = rospy.get_param("Limit_close_distance")
-	limit_mid_distance = rospy.get_param("Limit_mid_distance")
-	while not rospy.is_shutdown():
-		value = -1 
-		pub.publish(value)
+	if(data.find_ball):
+		rospy.loginfo(data.x_centre)
+		if (abs(data.x_centre)<20):
+			action = 1
+		if (data.x_centre > 20 and data.x_centre < 120):
+			action = 3
+		if (data.x_centre < -20 and data.x_centre > -120):
+			action = 2
+	if(not data.find_ball):
+		rospy.loginfo(data.x_centre) 
+		action = 10 
+	pub_action.publish(action)
+	pub_y_err.publish(data.y_centre)
 
 
 def listener_publisher():
