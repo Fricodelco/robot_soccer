43fce6c7e5387f5fd9ef49c89de1f1378f6c803f
Vladislav_Bakaev <bakaev.98@bk.ru>
2019-03-26 18:11:19 +0300

    add error y

--- robot_goalkeeper/src/goalkeeper_node.py
+++ robot_goalkeeper/src/goalkeeper_node.py
@@ -3,6 +3,9 @@ import roslib; roslib.load_manifest('robot_goalkeeper')
 import rospy
 from std_msgs.msg import Int16
 from ball_param_msg.msg import ball_param
+
+y = 0
+ 
 def callback(data):
 	pub_action = rospy.Publisher('forward_action', Int16, queue_size=10)
 	pub_y_err = rospy.Publisher('forward_y_err',Int16, queue_size=10)
@@ -16,11 +19,19 @@ def callback(data):
 			action = 3
 		if (data.x_centre < -20 and data.x_centre > -120):
 			action = 2
+		if (abs(data.y_centre)>30)
+			y = y + 0.03*data.y_centre
+			if(y>60)
+				y=60
+			else if(y<-60)
+				y=-60; 
+    }
 	if(not data.find_ball):
 		rospy.loginfo(data.x_centre) 
-		action = 10 
+		action = 10
+		y = 555
 	pub_action.publish(action)
-	pub_y_err.publish(data.y_centre)
+	pub_y_err.publish(y)
 
 
 def listener_publisher():
